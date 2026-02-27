f6ef0c8c8823944fad439d66bda80c9c11fcaad0
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2026-02-27 14:18:15 +0100

    Test all ones cases

--- src/numberToText/numberToText.test.ts
+++ src/numberToText/numberToText.test.ts
@@ -1,16 +1,13 @@
 import { describe, it, expect } from "vitest";
 import numberToText from "./numberToText";
+import { ones } from "./numberToText";
 
 describe("numberToText", () => {
-	it("converts 1 to eins", () => {
-		expect(numberToText(1)).toBe("eins");
-	});
-
-	it("converts 2 to zwei", () => {
-		expect(numberToText(2)).toBe("zwei");
-	});
-
-	it("converts 3 to drei", () => {
-		expect(numberToText(3)).toBe("drei");
+	describe("ones", () => {
+		Object.entries(ones).forEach(([number, text]) => {
+			it(`converts ${number} to ${text}`, () => {
+				expect(numberToText(Number(number))).toBe(text);
+			});
+		});
 	});
 });
