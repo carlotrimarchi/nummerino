5da60f656b9679770c2c26a80876e91c4a63fb52
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2026-02-28 10:21:06 +0100

    wip: working prototype of numberToText

--- src/numberToText/numberToText.test.ts
+++ src/numberToText/numberToText.test.ts
@@ -1,37 +1,105 @@
 import { describe, it, expect } from "vitest";
-import numberToText from "./numberToText";
+import { numberToText, getPlaceValues } from "./numberToText";
 import { ones, teens, tens, scales } from "./numberToText";
 
-describe("numberToText", () => {
-	describe("ones", () => {
-		Object.entries(ones).forEach(([number, text]) => {
-			it(`converts ${number} to ${text}`, () => {
-				expect(numberToText(Number(number))).toBe(text);
-			});
-		});
+describe("getPlaceValues", () => {
+	const cases: [number, number[]][] = [
+		[1, [1]],
+		[12, [2, 10]],
+		[123, [3, 20, 100]],
+		[1_234, [4, 30, 200, 1_000]],
+		[12_345, [5, 40, 300, 2_000, 10_000]],
+		[123_456, [6, 50, 400, 3_000, 20_000, 100_000]],
+		[1_234_567, [7, 60, 500, 4_000, 30_000, 200_000, 1_000_000]],
+		[
+			12_345_678,
+			[8, 70, 600, 5_000, 40_000, 300_000, 2_000_000, 10_000_000],
+		],
+	];
+	it.each(cases)("gets %i → %j", (input, expected) => {
+		expect(getPlaceValues(input as number)).toEqual(expected);
 	});
 
-	describe("teens", () => {
-		Object.entries(teens).forEach(([number, text]) => {
-			it(`converts ${number} to ${text}`, () => {
-				expect(numberToText(Number(number))).toBe(text);
+	describe("numberToText", () => {
+		describe("ones", () => {
+			it("1", () => {
+				expect(numberToText(1)).toBe("eins");
+			});
+
+			Object.entries(ones).forEach(([number, text]) => {
+				if (number === "1") return;
+				it(`converts ${number} to ${text}`, () => {
+					expect(numberToText(Number(number))).toBe(text);
+				});
 			});
 		});
+
+		// describe("teens", () => {
+		// 	Object.entries(teens).forEach(([number, text]) => {
+		// 		it(`converts ${number} to ${text}`, () => {
+		// 			expect(numberToText(Number(number))).toBe(text);
+		// 		});
+		// 	});
+		// });
+
+		// describe("tens", () => {
+		// 	Object.entries(tens).forEach(([number, text]) => {
+		// 		it(`converts ${number} to ${text}`, () => {
+		// 			expect(numberToText(Number(number))).toBe(text);
+		// 		});
+		// 	});
+		// });
+
+		// describe("scales", () => {
+		// 	Object.entries(scales).forEach(([number, text]) => {
+		// 		it(`converts ${number} to ${text}`, () => {
+		// 			expect(numberToText(Number(number))).toBe(text);
+		// 		});
+		// 	});
+		// });
+
+		// 	describe("10", () => {
+		// 		it("10 to zehn", () => {
+		// 			expect(numberToText(10)).toBe("zehn");
+		// 		});
+		// 	});
+
+		// 	describe("20", () => {
+		// 		it("20 to zwanzig", () => {
+		// 			expect(numberToText(20)).toBe("zwanzig");
+		// 		});
+		// 	});
+		// 	describe("23", () => {
+		// 		it("23 to drei und zwanzig", () => {
+		// 			expect(numberToText(23)).toBe("drei und zwanzig");
+		// 		});
+		// 	});
 	});
 
-	describe("tens", () => {
-		Object.entries(tens).forEach(([number, text]) => {
-			it(`converts ${number} to ${text}`, () => {
-				expect(numberToText(Number(number))).toBe(text);
-			});
-		});
+	it("1", () => {
+		expect(numberToText(1)).toBe("eins");
 	});
-
-	describe("scales", () => {
-		Object.entries(scales).forEach(([number, text]) => {
-			it(`converts ${number} to ${text}`, () => {
-				expect(numberToText(Number(number))).toBe(text);
-			});
-		});
+	it("109 ", () => {
+		expect(numberToText(109)).toBe("ein hundert neun");
 	});
+	it("119 ", () => {
+		expect(numberToText(119)).toBe("ein hundert neunzehn");
+	});
+	it("123 ", () => {
+		expect(numberToText(123)).toBe("ein hundert drei und zwanzig");
+	});
+	// it("223 ", () => {
+	// 	expect(numberToText(223)).toBe("zwei hundert drei und zwanzig");
+	// });
+	// it("1234 ", () => {
+	// 	expect(numberToText(1234)).toBe(
+	// 		"ein tausend zwei hundert vier und dreißig",
+	// 	);
+	// });
+	// it("555 ", () => {
+	// 	expect(numberToText(555)).toBe("fünf hundert fünf und fünfzig");
+	// });
+	// it("741 ", () => {
+	// 	expect(numberToText(741)).toBe("sieben hundert ein und vierzig");
+	// });
 });
--- src/numberToText/numberToText.ts
+++ src/numberToText/numberToText.ts
@@ -1,5 +1,5 @@
 export const ones: Record<number, string> = {
-	1: "eins",
+	1: "ein",
 	2: "zwei",
 	3: "drei",
 	4: "vier",
@@ -40,15 +40,93 @@ export const scales: Record<number, string> = {
 	1_000_000: "million",
 	1_000_000_000: "milliarde",
 	1_000_000_000_000: "billion",
-}
+};
+
+const scaleWords = ["hundert", "tausend", "million", "milliarde", "billion"];
 
 const numberGroups = [ones, teens, tens, scales];
 
-export default function numberToText(number: number): string {
-	for (const numberGroup of numberGroups) {
-		if (number in numberGroup) {
-			return numberGroup[number];
+export function numberToText(number: number): string {
+	if (number === 1) return "eins";
+	const placeValues = getPlaceValues(number).reverse();
+	console.log("placeValues", placeValues);
+	const result = [];
+
+	const stop = placeValues.length > 2 ? 2 : 0;
+
+	for (let i = 0; i < placeValues.length - stop; i++) {
+		console.log("result", result);
+		let value = placeValues[i];
+		const magnitude = Math.pow(10, Math.floor(Math.log10(value)));
+
+		if (magnitude >= 100) {
+			value = value / magnitude;
+			result.push(ones[value] + " " + scales[magnitude]);
+		} else {
+			for (const numberGroup of numberGroups) {
+				if (value in numberGroup) {
+					if (value === 1) {
+						result.push(numberGroup[value] + "s");
+					} else {
+						result.push(numberGroup[value]);
+					}
+				}
+			}
 		}
 	}
-	return "";
+
+	if (placeValues.length > 2) {
+		console.log("result", result);
+		const last = placeValues.length - 1;
+		const secondLast = placeValues.length - 2;
+		if (placeValues[secondLast] === 0 && placeValues[last] < 20) {
+			result.push(ones[placeValues[last]]);
+		} else if (placeValues[secondLast] && placeValues[secondLast] < 20) {
+			placeValues[secondLast] += placeValues[last];
+			result.push(teens[placeValues[secondLast]]);
+		} else {
+			result.push(
+				ones[placeValues[last]] +
+					" und " +
+					tens[placeValues[secondLast]],
+			);
+		}
+	}
+	console.log(result);
+	// for (const numberGroup of numberGroups) {
+	// 	if (number in numberGroup) {
+	// 		return numberGroup[number];
+	// 	}
+	// }
+	return result.join(" ");
+
+	// .reduce((acc, value, i, array) => {
+	// 	const split = value.split(" ");
+	// 	if (i === array.length - 1) {
+	// 	return acc;
+	// 	}
+	// 	if (i === array.length - 2) {
+	// 		acc += array[array.length - 1] + " und " + value
+	// 		return acc;
+	// 	}
+
+	// 	if (split[1] && scaleWords.includes(split[1])) {
+	// 		acc += value + " ";
+	// 	} else {
+	// 		acc += value + " und "
+	// 	}
+	// 	return acc;
+	// }, "");
+}
+
+export function getPlaceValues(number: number): number[] {
+	const result = [];
+	let place = 1;
+	while (number > 0) {
+		const digit = (number % 10) * place;
+		result.push(digit);
+		number = Math.floor(number / 10);
+		place *= 10;
+	}
+	return result;
 }
